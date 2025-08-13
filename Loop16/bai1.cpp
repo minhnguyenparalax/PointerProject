d607479629ed2de9bbb5eb6769711c26a8fab487
Minh Nguyen <nguyenquang123uvb@gmail.com>
2025-08-13 19:10:03 +0700

    update

---
+++ Loop16/bai1.cpp
@@ -0,0 +1,26 @@
+// Nhập một số nguyên dương n, in ra màn hình giá trị của
+// S = 1 + 2 + 3 + .... + n 
+// Ví dụ:
+// n = 4
+// S = 10
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int S = 0;
+    cout << "Nhap n: ";
+    cin >> n;
+    cout << "S = 1";
+    for(int i = 2; i <=n; i++)
+    {
+        cout << "+" << i;
+        S += i;
+    }
+    cout<< "\nTong cua S:" << S;
+    
+}
+
