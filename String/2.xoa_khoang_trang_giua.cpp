8ca96359fee242cefcbc39ff93d28f5e42706a9e
Minh Nguyen <nguyenquang123uvb@gmail.com>
2025-08-21 11:22:42 +0700

    .

---
+++ String/2.xoa_khoang_trang_giua.cpp
@@ -0,0 +1,37 @@
+// 27
+// Bài 2
+// Xóa khoảng trắng ở giữa
+// Ví dụ: Minh          Nguyen
+// Kết quả: Minh Nguyen
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str;
+    cout<<"Nhap chuoi: ";
+    getline(cin,str);
+
+    int i = 0; 
+    while(i<str.length())
+    {
+        if(str[i] == ' ' && str[i+1] == ' ')
+        {
+            str.erase(i, 1);
+        }
+        else
+        i++;
+    }
+
+    cout<<str;
+
+   
+
+}
+
+
+// Kết quả:
+// PS F:\C++\String> .\a.exe
+// Nhap chuoi: Minh                                Nguyen
+// Minh Nguyen
