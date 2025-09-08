9fc0cefe633a79199eb7202f741e736983f4da65
Minh Nguyen <nguyenquang123uvb@gmail.com>
2025-09-08 23:54:47 +0700

    .

--- Struct/lam_lai.cpp
+++ Struct/lam_lai.cpp
@@ -43,8 +43,8 @@ struct Product
 void show_menu_choice(int &choice);
 void chose(string label, int &choice);
 void show_list_product(string label, Product* &products, int size );
-void add_new_product(string label, Product *&products, int &size);
-void delete_product(string label,  Product *&products, int &size);
+void add_new_product(string label, Product* &products, int &size);
+void delete_product(string label,  Product* &products, int &size);
 
 
 //3.Main
@@ -151,12 +151,12 @@ void add_new_product(string label, Product* &products, int &size)
 }
 void delete_product(string label, Product *&products, int &size)
 {
-    cout<<"Nhap ten san pham can xoa: "<<endl;
+    cout<<label<<endl;
     string delete_name;
     cin.ignore();
     getline(cin, delete_name);
 
-    //Vòng for nhầm tên sản phầm
+    //Vòng for nhập tên sản phầm
     int delete_index = -1;
     for (int i = 0; i<size; i++)
     {
@@ -177,19 +177,19 @@ void delete_product(string label, Product *&products, int &size)
     {
         products[i] = products[i+1];
     }
+    size--;
     
-    
-    int new_size = size--;
+    int new_size = size;
     Product *tmp_products = new Product[new_size];
-    for(int i = 0; i<size-1; i++)
+    for(int i = 0; i<size; i++)
     {
         tmp_products[i] = products[i];
     }
 
     delete[] products;
     products = tmp_products;
-    size = new_size;
-
+    
     cout<<"Xoa thanh cong"<<endl;
+
 
 }
