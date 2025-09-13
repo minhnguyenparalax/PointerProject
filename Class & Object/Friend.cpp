02b0abbffaace2836c6c83dbb198c23ae34e1848
Minh Nguyen <nguyenquang123uvb@gmail.com>
2025-09-13 23:23:36 +0700

    .

--- Class & Object/Friend.cpp
+++ Class & Object/Friend.cpp
@@ -16,13 +16,41 @@ public:
     }
 
     friend void print_point(Point point);//Hàm bạn có quyền truy xuất đến thuộc tính private
+    //Hàm bạn ko phải member của class Point nên ko thừa kế được
+    // {
+    //     cout<< point.x << " " << point.y <<endl;
+    // }
+
+    friend Point operator+(Point p1, Point p2)//Để truy cập x và y trong private
+    {
+        Point p3;
+        p3.x = p1.x + p2.x;
+        p3.y = p1.y + p2.y;
+
+        return p3;
+    }
 };
 
 void print_point(Point point)
 {
     cout<< point.x << " " << point.y <<endl;
 }
+
+// Point operator+(Point p1, Point p2)//Để truy cập x và y trong private
+//{
+//         Point p3;
+//         p3.x = p1.x + p2.x;
+//         p3.y = p1.y + p2.y;
+
+//         return p3;
+//}
+
+
+
 int main()
 {
-    print_point(Point(1,2));
+    Point p1(1,2), p2(3,4);
+    Point p3 = p1 + p2;
+
+    print_point(p3);
 }
